db07a46d8f582c1d4084fea401dba8ef608bd48b
Kleber Sanzony <69324224+kleberrsanzony@users.noreply.github.com>
2026-04-16 22:18:17 -0300

    chore(config): simplify next.config for production stability

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  compress: true,
-  poweredByHeader: false,
-  reactStrictMode: true,
-  allowedDevOrigins: ['192.168.3.152', '192.168.1.12'],
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
